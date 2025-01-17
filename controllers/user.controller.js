cc7d223a33ab007e18054ad3d307ab591f3640b0
guilhermegoedtel <guilhermegoedtel@gmail.com>
2025-01-17 11:15:49 -0300

    GET users com ID

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,4 +7,12 @@ router.get('/', async (req, res) => {
     res.send(users)
 })
 
+router.get('/:id', async (req, res) => {
+    const user = await service.getuserById(req.params.id)
+    if (user == undefined)
+        res.status(404).json('Não há registros com o id: ' + req.params.id)
+    else
+        res.send(user)
+})
+
 module.exports = router;
