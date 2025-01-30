cbbbf96b94961f03c04d7904690b59c2e0b1da29
guilhermegoedtel <guilhermegoedtel@gmail.com>
2025-01-30 14:47:16 -0300

    token jwt

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express'), app = express(), bodyparser = require('body-
 require('express-async-errors')
 
 userRoutes = require('./controllers/user.controller')
+loginRoutes = require('./controllers/login.controller')
 
 app.use(bodyparser.json())
+
 app.use('/api/users', userRoutes)
+app.use('/api/login', loginRoutes)
+
 app.use((err, req, res, next) => {
     console.log(err)
     res.status(err.status || 500).send('Something went wrong!')
