cd4aa26b7822e1a7bf323ed7cb53b7113e5bc5b6
guilhermegoedtel <guilhermegoedtel@gmail.com>
2025-02-14 12:40:15 -0300

    ajuste de cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express'), app = express(), bodyparser = require('body-parser');
 
+var cors = require('cors')
+
 require('express-async-errors')
 
 userRoutes = require('./controllers/user.controller')
 loginRoutes = require('./controllers/login.controller')
 uploadRoutes = require('./controllers/upload.controller')
+
+app.use(cors())
 
 app.use(bodyparser.json())
 
